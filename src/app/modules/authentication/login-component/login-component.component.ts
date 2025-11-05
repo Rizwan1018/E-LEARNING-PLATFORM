5d26af93dd3173bc761d856ab72ae4918615e522
Aditya Singh <adityasinghsisodiya2003@gmail.com>
2025-11-05 10:59:18 +0530

    fixed the popup ts file login module

--- src/app/modules/authentication/login-component/login-component.component.ts
+++ src/app/modules/authentication/login-component/login-component.component.ts
@@ -15,11 +15,62 @@ export class LoginComponentComponent implements OnInit {
 
   
 
+  // loginForm!: FormGroup;
+
+  // constructor(
+  //   private formBuilder: FormBuilder,
+  //   private authService: AuthService, 
+  //   private router: Router
+  // ) {}
+
+  // ngOnInit(): void {
+  //   this.loginForm = this.formBuilder.group({
+  //     email: ['', [Validators.required, Validators.email]],
+  //     password: ['', [Validators.required]]
+  //   });
+  // }
+
+  // login(): void {
+  //   if (this.loginForm.valid) {
+  //     this.authService.login(this.loginForm.value).subscribe({
+  //       next: (response) => {
+  //         // alert('Login successful!');
+  //         const successModal = new bootstrap.Modal(document.getElementById('statusSuccessModal'));
+  //          successModal.show();
+
+  //         console.log('Response:', response);
+
+  //      //   localStorage.setItem('user', JSON.stringify(response));
+          
+  //         if (response.role === 'STUDENT') {
+  //           this.router.navigate(['student'], { queryParams: { id: response.id } });
+  //         } else if (response.role === 'INSTRUCTOR') {
+  //           this.router.navigate(['instructor'], { queryParams: { id: response.id } });
+  //         } else {
+  //           this.router.navigate(['home']);
+  //         }
+  //       },
+  //       error: (err) => {
+  //         console.log(err)
+  //         // alert('Invalid credentials');
+  //         const errorModal = new bootstrap.Modal(document.getElementById('statusErrorsModal'));
+  //          errorModal.show();
+
+  //       }
+  //     });
+  //   }
+  // }
+
+  // get f() {
+  //   return this.loginForm.controls;
+  // }
+
+  
   loginForm!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
-    private authService: AuthService, 
+    private authService: AuthService,
     private router: Router
   ) {}
 
@@ -31,31 +82,44 @@ export class LoginComponentComponent implements OnInit {
   }
 
   login(): void {
-    if (this.loginForm.valid) {
-      this.authService.login(this.loginForm.value).subscribe({
-        next: (response) => {
-          // alert('Login successful!');
-          new bootstrap.Modal(document.getElementById('statusSuccessModal')).show();
-          console.log('Response:', response);
+  if (this.loginForm.valid) {
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (response) => {
+        console.log('Login response:', response);
 
-       //   localStorage.setItem('user', JSON.stringify(response));
-          
-          if (response.role === 'STUDENT') {
-            this.router.navigate(['student'], { queryParams: { id: response.id } });
-          } else if (response.role === 'INSTRUCTOR') {
-            this.router.navigate(['instructor'], { queryParams: { id: response.id } });
-          } else {
-            this.router.navigate(['home']);
+        const modalEl = document.getElementById('statusSuccessModal');
+        if (modalEl) {
+          const successModal = new bootstrap.Modal(modalEl);
+          successModal.show();
+
+          // Wait for user to click "Ok"
+          const okBtn = document.getElementById('successModalOkBtn');
+          if (okBtn) {
+            okBtn.addEventListener('click', () => {
+              if (response.role === 'STUDENT') {
+                this.router.navigate(['student'], { queryParams: { id: response.id } });
+              } else if (response.role === 'INSTRUCTOR') {
+                this.router.navigate(['instructor'], { queryParams: { id: response.id } });
+              } else {
+                this.router.navigate(['home']);
+              }
+            }, { once: true }); // ensures listener runs only once
           }
-        },
-        error: (err) => {
-          console.log(err)
-          // alert('Invalid credentials');
-          new bootstrap.Modal(document.getElementById('statusErrorsModal')).show()
         }
-      });
-    }
+      },
+      error: (err) => {
+        console.error('Login error:', err);
+
+        const errorEl = document.getElementById('statusErrorsModal');
+        if (errorEl) {
+          const errorModal = new bootstrap.Modal(errorEl);
+          errorModal.show();
+        }
+      }
+    });
   }
+}
+
 
   get f() {
     return this.loginForm.controls;
